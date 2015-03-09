6840789f7574f6f2f836803a6ec10ac3a21f3e60
pqz317 <patrickqz@yahoo.com>
2015-03-08 21:09:29 -0700

    added frequency loop to main, looping through 1 - 23 hZ currently

--- Main.m
+++ Main.m
@@ -1,9 +1,19 @@
 %  Neuroeng BCI algorthim implementation
-
+% loading data from files, change URL accordingly
 data = load('\\cseexec.cs.washington.edu\cs\nt\homes\istudents\nhlien93\490n\BCICIV_calib_ds1a.mat');
 cnt = 0.1 * double(data.cnt);
-[a, b] = butterTwoBp(1/100, 10, 15);
-Y = zeros(190594, 59);
-for n = 1:59
-    Y(:, n) = filter(a, b, cnt(:,n));
+
+% loop through all important frequency bands
+% starting from 1 HZ, ending at 23 HZ
+lo = 1;
+hi = 3;
+while hi <= 23
+    [a, b] = butterTwoBp(1/100, lo, hi);
+    Y = zeros(190594, 59);
+    for n = 1:59
+        Y(:, n) = filter(a, b, cnt(:,n));
+    end
+    hi = hi + 2;
+    lo = lo + 2;
 end
+% Y is a filtered matrix

f145ccb415646683f8ddcede34d5fbe61192ed6b
Patrick Zhang <pqz317@attu1.cs.washington.edu>
2015-03-12 15:52:44 -0700

    have spliting matrices working

--- Main.m
+++ Main.m
@@ -12,8 +12,12 @@ data.cnt = 0.1 * double(data.cnt);
 % starting from 1 HZ, ending at 23 HZ
 
 %prepare data for discerning ERS/ERD
-[] = GetWeightsAndFrequencies(cnt, classifier)
+e_class = abs(classifier);
+[e_lo, e_hi, e_cspW, e_ldaW] = GetWeightsAndFrequencies(cnt, e_class);
 
-%perpare data for discerning different movements
-[] = GetWeightsAndFrequencies(cnt, classifier)
+%perpare data for discerning different movements\
+split = arrayfun(@(x) cnt(classifier == x, :), unique(classifier), 'uniformoutput', false);
+
+m_cnt = [split{1};split{3}];
+[m_lo, m_hi, m_cspW, m_ldaW] = GetWeightsAndFrequencies(cnt, classifier);
 
